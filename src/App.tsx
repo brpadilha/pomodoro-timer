01ba19e15fca207777e46783afca8761dc38eb24
Bruno <brpadilha.dev@gmail.com>
2023-04-17 15:31:46 -0300

    cria theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from "styled-components";
+
 export function App() {
-  return <h1>Hello</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button />
+    </ThemeProvider>
+  );
 }
 
 export default App;
